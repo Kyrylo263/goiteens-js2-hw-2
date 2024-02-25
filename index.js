bbc22861677eb32a20397c798c6fd133f9181994
Kyrylo263 <kyryloz2000@gmail.com>
2024-02-25 09:04:00 +0200

    add1

--- index.js
+++ index.js
@@ -14,3 +14,11 @@ function changeSizeImage(event) {
 
 // 2.
 
+const box = document.getElementById('box');
+
+function moveBox(event) {
+    box.style.left = `${event.clientX}px`;
+    box.style.top = `${event.clientY}px`;
+};
+
+document.addEventListener('mousemove', _.debounce(moveBox, 100))
